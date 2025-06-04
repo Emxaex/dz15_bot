050d2c1b48d286e4977220fa54df64252adfd0cb
Emxaex <mrmaxexe@gmail.com>
2025-06-04 15:45:35 +0500

    Ошибка в коде

---
+++ TeleBot.py
@@ -0,0 +1,43 @@
+import telebot
+import database
+
+menu1 = telebot.types.InlineKeyboardMarkup()
+menu1.row(
+    telebot.types.InlineKeyboardButton('Проживающие', callback_data='help'),
+    telebot.types.InlineKeyboardButton('Зарегистрироваться', callback_data='register')
+    )
+
+bot=telebot.TeleBot('')
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.from_user.id, f'Добро пожаловать \n'
+                                           f'Выберите что хотите сделать', reply_markup=menu1 )
+
+#В общем это оброботчик когда человек нажмёт на inline кнопку. call это просто имя переменной
+#которое может быть любое. Но у программистов принято называть call
+#можно задать ещё условие по типу
+#@bot.callback_query_handler(func=lambda call: call.data == 'register')
+#этот обработчик следит чтобы именно на register нажали, а help он будет игнорировать
+@bot.callback_query_handler(func=lambda call: True)
+def callback_handler(call):
+    if call.data=='help':
+        info = database.show_info()
+        bot.send_message(call.message.chat. id, str(info))
+      #  bot.register_next_step_handler(call.message,help_info)
+    elif call.data=='register':
+        bot.send_message(call.message.chat.id, 'Введите ваше имя, номер телефона и номер желаемой комнаты через пробел')
+        bot.register_next_step_handler(call.message,register)
+def help_info(message):
+    bot.send_message(message.from_user.id, database.show_info)
+
+def register(message):
+    print(message)
+    user_info = message.text.strip()
+    #берёт значение из user_info и разделяет его
+    user_name,user_number,hotel_number = [x.strip() for x in user_info.split(' ')]
+    database.add_client(user_name,user_number,hotel_number)
+    bot.send_message(message.from_user.id, "Ты успешно зарегистрирован! Спасибо!")
+
+
+
+bot.polling(non_stop=True)
---
+++ database.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+sql=sqlite3.connect('Register_Hostel')
+base=sql.cursor()
+
+base.execute("""CREATE TABLE IF NOT EXISTS register (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    tel_number TEXT,
+    otel_num INTEGER
+); """)
+
+def add_client(name, tel_number, otel_num):
+    base.execute("""INSERT INTO register (name,tel_number,otel_num) VALUES(?,?,?); """,(name,tel_number,otel_num))
+    base.commit()
+def show_info():
+    return base.execute("""SELECT * FROM register """).fetchall()
+
+
+
+#CREATE	Создание таблиц
+#INSERT	Вставка новых данных
+#SELECT	Чтение данных
+#UPDATE	Обновление существующих строк
+#DELETE	Удаление строк
+#DROP	Удаление таблиц
+#ALTER	Изменение структуры таблицы
+#COUNT, SUM, AVG и т.д.	Статистика по данным
+#WHERE, ORDER BY, LIMIT	Условия и сортировка
